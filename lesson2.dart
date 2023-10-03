3aa8172b19745118b81beb3630886fbb7fadbf66
abylay-11 <113657296+abylay-11@users.noreply.github.com>
2023-10-03 20:37:14 +0500

    Lesson 2. Variable and data types

---
+++ lesson2.dart
@@ -0,0 +1,32 @@
+void main() {
+  // Переменные и типы данных
+  var title = 'Курс Dart видео 2'; // var type string
+  title = 'С нуля до профи Dart Переменные #2';
+
+  var likes = 99999; // var type integer
+  likes = 100000;
+
+  var raiting = 4.5; // var type double
+  raiting = 4.0;
+
+  var great = true; // var type boolean
+  great = false;
+
+  // Указание типа
+  String title2 = "Это новый заголовок видео";
+  int likes2 = 10;
+  double raitng2 = 3.0;
+  bool great2 = false;
+
+  print(title);
+  print(likes);
+  print(raiting);
+  print(great);
+
+  print('---');
+
+  print(title2);
+  print(likes2);
+  print(raitng2);
+  print(great2);
+}
