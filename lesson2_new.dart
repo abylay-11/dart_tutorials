9650450fbc9f7aa20917faf27d00296430392c66
abylay-11 <113657296+abylay-11@users.noreply.github.com>
2023-12-21 19:20:21 +0500

    Lesson 2 again

---
+++ lesson2_new.dart
@@ -0,0 +1,37 @@
+void main() {
+  // Var -> string
+  var title = 'Курс Dart видео 2';
+  title = 'С нуля до профи Dart Переменные #2';
+  // String
+  String title2 = 'Some string';
+
+  // Var -> int
+  var likes = 99999;
+  likes = 100000;
+  // Int
+  int likes2 = 1000;
+
+  // Var -> double
+  var rating = 4.5;
+  rating = 4.0;
+  // Double
+  double rating2 = 4.6;
+
+  // Var -> bool
+  var great = true;
+  great = false;
+  // Bool
+  bool great2 = false;
+
+  print(title);
+  print(title2);
+
+  print(likes);
+  print(likes2);
+
+  print(rating);
+  print(rating2);
+
+  print(great);
+  print(great2);
+}
